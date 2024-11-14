a9794bd0a82f45de63604d1658550416f70e6e1e
Zyncol <isaacyilipochipofya@gmail.com>
2024-11-14 12:42:11 +0200

    test monetise

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,7 +19,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """ 
+        """
         Test that the method returns the valid
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
